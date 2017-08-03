e55ac56757022ca6fb8f7bfabd2ea3856ae3d806
Gabriel Antonius <gabriel.antonius@gmail.com>
2017-08-03 11:04:09 -0700

    Added an iterator to Workflow.

--- abitools/core/workflow.py
+++ abitools/core/workflow.py
@@ -148,3 +148,15 @@ class Workflow(Task):
 
     def clear_tasks(self):
         del self.tasks[:]
+
+    def __iter__(self):
+        """
+        Iterate over tasks. If a task is a workflow, continue iterating
+        over the tasks of the workflow.
+        """
+        for task in self.tasks:
+            if '__iter__' in dir(task):
+                for subtask in task:
+                    yield subtask
+            else:
+                yield task
